b51ab09dfeb42e36abcd5cfee6e9d4ceda369413
Azura974 <eleahyou974@gmail.com>
2023-04-09 22:19:19 +0400

    modified buttons n other fixes

--- things.js
+++ things.js
@@ -4,6 +4,8 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     document.body.classList.add("light-mode")
 }
 
+
+
 const themechanger = document.getElementById("themechanger")
 themechanger.addEventListener("click", ChangeTheme, {
     passive: true
@@ -22,50 +24,69 @@ function ChangeTheme() {
 }
 
 const carousel = document.getElementById("carousel")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
-const carouselItems = carousel.querySelectorAll("ol li")
-let currentItem = 0
-
-prev.addEventListener("click", prevItem)
-next.addEventListener("click", nextItem)
-
 let interval
+if (carousel) {
+    const prev = document.getElementById("prev")
+    const next = document.getElementById("next")
+    const carouselItems = carousel.querySelectorAll("ol li")
+    let currentItem = 0
+    prev.addEventListener("click", prevItem)
+    next.addEventListener("click", nextItem)
 
-function carouselAutoplay(){
-    interval = setInterval(nextItem,4000)
-}
+    
 
-carouselAutoplay()
-
-function prevItem() {
-    clearInterval(interval)
-    carouselAutoplay()
-    currentItem -= 1
-    if (currentItem < 0) currentItem = carouselItems.length-1
-    setActiveItem(true)
-}
-
-function nextItem() {
-    clearInterval(interval)
-    carouselAutoplay()
-    currentItem += 1
-    if (currentItem > carouselItems.length-1) currentItem = 0
-    setActiveItem()
-}
-
-function setActiveItem(left = false) {
-
-    carouselItems[currentItem].classList.add(left ? "after-active" : "before-active")
-    for (item of carouselItems) {
-        item.classList.replace("active",left ? "before-active" : "after-active")
+    function carouselAutoplay() {
+        interval = setInterval(nextItem, 4000)
     }
-    setTimeout(() => {
-        carouselItems[currentItem].classList.replace(left ? "after-active" : "before-active","active")
-    },5)
-    setTimeout(() => {
+
+    carouselAutoplay()
+
+    function prevItem() {
+        clearInterval(interval)
+        carouselAutoplay()
+        currentItem -= 1
+        if (currentItem < 0) currentItem = carouselItems.length - 1
+        setActiveItem(true)
+    }
+
+    function nextItem() {
+        clearInterval(interval)
+        carouselAutoplay()
+        currentItem += 1
+        if (currentItem > carouselItems.length - 1) currentItem = 0
+        setActiveItem()
+    }
+
+    function setActiveItem(left = false) {
+
+        carouselItems[currentItem].classList.add(left ? "after-active" : "before-active")
         for (item of carouselItems) {
-            item.classList.remove(left ? "before-active" : "after-active")
+            item.classList.replace("active", left ? "before-active" : "after-active")
         }
-    },500)
+        setTimeout(() => {
+            carouselItems[currentItem].classList.replace(left ? "after-active" : "before-active", "active")
+        }, 5)
+        setTimeout(() => {
+            for (item of carouselItems) {
+                item.classList.remove(left ? "before-active" : "after-active")
+            }
+        }, 500)
+    }
+}
+document.addEventListener('scroll', AfficheBouton, {
+    passive: true
+})
+const bouton = document.getElementById("bouton")
+
+function AfficheBouton() {
+    if (carousel) clearInterval(interval)
+    if (scrollY > 5) {
+        bouton.classList.remove("inactif")
+        bouton.classList.add("actif")
+    }
+    if (scrollY < 5) {
+        if (carousel) carouselAutoplay()
+        bouton.classList.remove("actif")
+        bouton.classList.add("inactif")
+    }
 }
